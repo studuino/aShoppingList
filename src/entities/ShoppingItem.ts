85e2784076889cd28a98c96ac770ad1f6a082105
Adam Hansen <adamlars90@gmail.com>
2018-05-27 15:55:15 +0200

    Added list item quantity

---
+++ src/entities/ShoppingItem.ts
@@ -0,0 +1,7 @@
+export interface ShoppingItem {
+  uid?: string;
+  title: string;
+  note?: string;
+  icon?: string;
+  quantity: number;
+}
--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 import {DetailItemPage} from './detail-item/detail-item';
+import {ShoppingItem} from '../../entities/ShoppingItem';
 
 @Component({
   selector: 'page-shopping-list',
@@ -8,7 +9,7 @@ import {DetailItemPage} from './detail-item/detail-item';
 })
 export class ShoppingListPage {
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items: Array<ShoppingItem>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // Let's populate this page with some filler content for funzies
@@ -19,7 +20,8 @@ export class ShoppingListPage {
     for (let i = 1; i < 11; i++) {
       this.items.push({
         title: 'Item ' + i,
-        note: 'This is item #' + i,
+        note: 'Note for item #' + i,
+        quantity: Math.floor(Math.random() * this.items.length) + 1,
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
