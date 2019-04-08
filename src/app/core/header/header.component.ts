943678a660cf22c7d6640ced6e4a6a118f0b312a
Adam Hansen <adamlars90@gmail.com>
2019-04-08 20:58:46 +0200

    Updated styling
    - Improved desktop setup,
    - Changed theme colors

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ModuleRoutes } from '../../ModuleRoutes';
+import { MenuController, NavController, Platform } from '@ionic/angular';
+import { AuthService } from '../../auth/shared/auth.service';
 
 @Component({
   selector: 'a-header',
@@ -12,10 +16,38 @@ export class HeaderComponent implements OnInit {
   @Input()
   shouldDisplayBackButton = false;
 
-  constructor() {
+  browserMode = false;
+
+  constructor(private router: Router,
+              private platform: Platform,
+              private navCtrl: NavController,
+              private menuCtrl: MenuController,
+              private authService: AuthService) {
+    if (this.platform.is('desktop')) {
+      this.browserMode = true;
+    }
   }
 
   ngOnInit() {
   }
 
+  navigateToShopping() {
+    this.router.navigateByUrl(ModuleRoutes.SHOPPING_LIST);
+  }
+
+  isShoppingUrl() {
+    return window.location.href.includes(ModuleRoutes.SHOPPING_LIST);
+  }
+
+  navigateToCategories() {
+    this.router.navigateByUrl('/category');
+  }
+
+  logout() {
+    this.navCtrl.navigateRoot(ModuleRoutes.LOGIN)
+      .then(() => {
+        this.menuCtrl.enable(false);
+        this.authService.logout();
+      });
+  }
 }
